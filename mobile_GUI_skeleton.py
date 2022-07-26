8b4f493612a2ef7a04f8eb6a0466a153e4449842
millahan <101675604+millahan@users.noreply.github.com>
2022-07-26 20:41:51 +1200

    Add files via upload

--- mobile_GUI_skeleton.py
+++ mobile_GUI_skeleton.py
@@ -1,85 +1,112 @@
 from tkinter import *
 from functools import partial #To prevent unwanted windows
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image #To present logo
 
 class MobileService:
     def __init__(self,parent):
 
-        #GUI to get starting balance and stakes
-        self.start_frame = Frame(padx=10, pady=10)
-        self.start_frame.grid()
+        #Formatting Variables
+        background_colour = "red"
 
-        #Mystery box heading (row 0)
-        self.mystery_box_label = Label(self.start_frame,
-                                        text = "Mystery Box Game",
-                                            font="Arial 19 bold")
-        self.mystery_box_label.grid(row=1)
+        # Service main screen GUI
+        self.GUI_frame = Frame(width=300, height=300, bg=background_colour,
+                                     pady=10)
+        self.GUI_frame.grid()
 
-        #Entry Box (row 1)
-        self.start_amount_entry = Entry(self.start_frame,
-                                      font="Arial 16 bold")
-        self.start_amount_entry.grid(row=2)
+        #Mobile Service Logo (row 0)
+        self.mobile_service_label = Label(self.GUI_frame, text = "Mobile Service",
+                                            font="Arial 19 bold",
+                                          bg=background_colour,
+                                          padx=10, pady=10)
+        self.mobile_service_label.grid(row=0)
 
-        #Play Button (row 2)
-        self.lowstakes_button = Button(text="Low ($5)",
-                                  command=lambda: self.to_game(1))
-        self.lowstakes_button.grid(row=2, pady=10)
+        #Imformation Text (row 1)
+        self.information_label = Label(self.GUI_frame,
+                                       text = "Help text here",
+                                      font="Arial 10 bold",
+                                       wrap=250, justify=LEFT,
+                                       fg = "maroon", bg=background_colour,
+                                       padx=10, pady=10)
+        self.information_label.grid(row=1)
 
-    def to_game(self, stakes):
-        starting_balance = self.start_amount_entry.get()
-        Game(self, stakes, starting_balance)
+        #Enter Job Button (row 2)
+        self.enter_job_button = Button(self.GUI_frame, text="Enter New Job",
+                                  command=lambda:self.to_job())
+        self.enter_job_button.grid(row=2, column=0)
 
-class Game:
-    def __init__(self, partner, stakes, starting_balance):
-        print(stakes)
-        print(starting_balance)
+        #History Button (row 2)
+        self.history_button = Button(self.GUI_frame, text="Job History",
+                                  command=lambda:self.to_history())
+        self.history_button.grid(row=2, column=1)
 
-        # disable low stakes button
-        partner.lowstakes_button.config(state=DISABLED)
+    def to_job(self):
+        EnterJob(self)
 
-        #initialise variables
-        self.balance = IntVar()
+    def to_history(self):
+        History(self)
 
-        #Set starting balance to amount entered by user at start of game
-        self.balance.set(starting_balance)
+class EnterJob:
+    def __init__(self, partner):
 
-        #GUI setup
-        self.game_box = Toplevel()
-        self.game_frame = Frame(self.game_box)
-        self.game_frame.grid()
-
-        #Heading Row
-        self.heading_label = Label(self.game_frame, text="Heading",
-                                   font="Arial 24 bold", padx=10, pady=10)
-        self.heading_label.grid(row=0)
-
-        #Balance Label
-        self.balance_frame = Frame(self.game_frame)
-        self.balance_frame.grid(row=1)
-
-        self.balance_label = Label(self.game_frame, text="Balance...")
-        self.balance_label.grid(row=2)
-
-        self.play_button = Button(self.game_frame, text="Gain",
-                                  padx=10, pady=10, command=self.reveal_boxes)
-        self.play_button.grid(row=3)
-
-    def reveal_boxes(self):
-        #retrieve the balance from the initial function
-        current_balance = self.balance.get()
-
-        #Adjust the balance (subtract game cost and add pay out)
-        #For testing purposes, just add 2
-        current_balance += 2
-
-        #Set balance to adjusted balance
-        self.balance.set(current_balance)
-
-        #Edit label so user can see their balance
-        self.balance_label.configure(text="Balance: {}".format(current_balance))
-
+        #Disable New Job Button
+        partner.enter_job_button.config(state=DISABLED)
         
+        #Job Window
+        self.job_box = Toplevel()
+
+        #Close Job Window
+        self.job_box.protocol('WM_DELETE_WINDOW', partial(self.close_job, partner))
+
+        #GUI Frame Set Up
+        self.job_frame = Frame(self.job_box)
+        self.job_frame.grid()
+
+        #Set Up Heading (row 0)
+        self.job_heading = Label(self.job_frame, text = "Enter New Job Details",
+                                 font="arial 19 bold", padx=10, pady=10)
+        self.job_heading.grid(row=0)
+
+    def close_job(self,partner):
+        partner.enter_job_button.config(state=NORMAL)
+        self.job_box.destroy()
         
+class History:
+    def __init__(self, partner):
+
+        #Disable New Job Button
+        partner.history_button.config(state=DISABLED)
+        
+        #Job Window
+        self.history_box = Toplevel()
+
+        #Close Job Window
+        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+
+        #GUI Frame Set Up
+        self.history_frame = Frame(self.history_box)
+        self.history_frame.grid()
+
+        #Set Up Heading (row 0)
+        self.history_heading = Label(self.history_frame, text = "Job History",
+                                 font="arial 19 bold", padx=10, pady=10)
+        self.history_heading.grid(row=0)
+
+        #Show All Jobs Button (row 2)
+        self.show_all = Button(self.history_frame, text="Show All")
+        self.show_all.grid(row=2, column=0)
+
+        #Previous Job Button (row 2)
+        self.previous_button = Button(self.history_frame, text="Previous")
+        self.previous_button.grid(row=2, column=1)
+
+        #Next Job Button (row 2)
+        self.next_button = Button(self.history_frame, text="Next")
+        self.next_button.grid(row=2, column=2)
+
+    def close_history(self,partner):
+        partner.history_button.config(state=NORMAL)
+        self.history_box.destroy()
+
 #main routine
 if __name__ == "__main__":
     root = Tk()
